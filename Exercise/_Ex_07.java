9ce36d52e40229fc71b5d21cfbb5b90430601485
StarHawkMK1 <118034465+StarHawkMK1@users.noreply.github.com>
2023-03-01 14:29:08 +0900

    Add files via upload

---
+++ Exercise/_Ex_07.java
@@ -0,0 +1,12 @@
+package Star;
+
+public class _Ex_07 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j <= i+8; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Exercise/_Ex_09.java
@@ -0,0 +1,16 @@
+package Star;
+
+public class _Ex_09 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if ((j >= i && i + j <= 6) || (i >= j && i + j >= 6)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
